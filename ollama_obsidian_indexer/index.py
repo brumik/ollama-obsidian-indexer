6a47fbcd69a02398f5e398bfe1b225351aaa800c
Levente Berky <levente.berky@gmail.com>
2024-05-20 09:50:22 +0200

    Fix development mode switch

--- ollama_obsidian_indexer/index.py
+++ ollama_obsidian_indexer/index.py
@@ -5,9 +5,9 @@ import os
 
 def main():
     load_dotenv()
-    debug = os.getenv("APP_DEVELOPMENT", False)
+    debug = os.getenv("APP_DEVELOPMENT", 0)
     port = int(os.getenv("APP_PORT", 5000))
-    if debug:
+    if debug == 1:
         app.run(host="0.0.0.0", debug=True, port=port)
     else:
         serve(app, host='0.0.0.0', port=port)
