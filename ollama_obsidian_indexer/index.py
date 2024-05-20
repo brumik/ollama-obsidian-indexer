c0566b154b31984e1c008b81d79e7dbe055b3eff
Levente Berky <levente.berky@gmail.com>
2024-05-20 10:44:34 +0200

    Fix development env varaible string issue

--- ollama_obsidian_indexer/index.py
+++ ollama_obsidian_indexer/index.py
@@ -5,13 +5,12 @@ import os
 
 def main():
     load_dotenv()
-    debug = os.getenv("APP_DEVELOPMENT", 0)
+    debug = os.getenv("APP_DEVELOPMENT", "0")
     port = int(os.getenv("APP_PORT", 5000))
-    if debug == 1:
-        app.run(host="0.0.0.0", debug=True, port=port)
-    else:
+    if debug != "0":
         serve(app, host='0.0.0.0', port=port)
-
+    else:
+        app.run(host="0.0.0.0", debug=True, port=port)
 
 if __name__ == "__main__":
     main()
